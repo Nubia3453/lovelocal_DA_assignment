7c267f2e34e79921d0782a2714219d11e816626b
Vinayak Beldar <130481662+Nubia3453@users.noreply.github.com>
2025-04-06 14:53:47 +0530

    Add files via upload

---
+++ sql_codes/sql_questions.sql
@@ -0,0 +1,81 @@
+use transactional_data;
+
+-- Total revenue per customer.
+SELECT 
+    c.name AS 'Customer Name',
+    SUM(total_amount) AS 'Total Revenu'
+FROM
+    orders o
+        JOIN
+    customers c ON o.customer_id = c.customer_id
+GROUP BY c.name
+ORDER BY SUM(total_amount) DESC;
+    
+-- Average order value per product category.
+SELECT 
+    p.category AS 'Product Category',
+    ROUND(AVG(total_amount), 2) AS 'Avg order value'
+FROM
+    orders o
+        JOIN
+    order_items oi ON o.order_id = oi.order_id
+        JOIN
+    products p ON oi.product_id = p.product_id
+GROUP BY category
+ORDER BY AVG(total_amount) DESC;
+
+-- Total number of orders by customer demographics (e.g., age, location).
+
+SELECT 
+    (TIMESTAMPDIFF(YEAR,
+        c.birthdate,
+        CURRENT_DATE)) AS 'Customer Age',
+    c.location AS 'Customer Location',
+    COUNT(order_id) AS 'Total Orders'
+FROM
+    customers c
+        LEFT JOIN
+    orders o ON c.customer_id = o.customer_id
+GROUP BY c.location , (TIMESTAMPDIFF(YEAR,
+    c.birthdate,
+    CURRENT_DATE));
+
+
+
+-- Average customer rating per product.
+SELECT 
+    *
+FROM
+    customer_reviews;
+SELECT 
+    product_name AS 'Product',
+    ROUND(AVG(rating), 1) AS 'Average rating'
+FROM
+    products p
+        JOIN
+    customer_reviews cr ON p.product_id = cr.product_id
+GROUP BY product_name
+ORDER BY AVG(rating) DESC;
+
+-- Identify customers who have the highest potential for up-sell or 
+-- cross-sell opportunities based on their past purchase behavior (e.g., frequent purchasers and high-value customers).
+-- frequnt purchase - highest order count
+-- high value - most spent amount 
+
+SELECT 
+    c.name AS 'Customer Name',
+    COUNT(o.order_id) AS 'Total Orders',
+    SUM(o.total_amount) AS 'Total Spendings',
+    TIMESTAMPDIFF(YEAR,
+        c.signup_date,
+        CURRENT_DATE) AS 'Customer Age in Years'
+FROM
+    orders o
+        LEFT JOIN
+    customers c ON o.customer_id = c.customer_id
+GROUP BY c.name , c.signup_date
+ORDER BY COUNT(order_id) DESC , SUM(total_amount) DESC , TIMESTAMPDIFF(YEAR,
+    c.signup_date,
+    CURRENT_DATE) DESC
+LIMIT 5;
+
---
+++ sql_codes/table_creation_script.sql
@@ -0,0 +1,55 @@
+create schema transactional_data;
+
+use transactional_data;
+
+create table customers (
+	customer_id smallint primary key,
+    name varchar(32),
+    email varchar(32),
+    location varchar(32),
+    signup_date date not null,
+    birthdate date not null);
+
+create table orders (
+	  order_id smallint primary key AUTO_INCREMENT,
+      customer_id smallint not null,
+	  order_date date not null,
+      total_amount decimal(5,2) not null,
+      status varchar(16),
+      foreign key (customer_id) references customers(customer_id) );
+    
+create table products (
+	product_id smallint primary key,
+    product_name varchar(32) not null,
+    category varchar(32) not null,
+    price decimal(5,2) not null
+	);
+    
+create table order_items ( 
+	order_id smallint not null,
+    product_id smallint not null,
+    quantity smallint not null,
+    foreign key(order_id) references orders(order_id),
+    foreign key(product_id) references products(product_id)) ;
+    
+create table customer_reviews (
+		review_id smallint primary key,
+        order_id smallint not null,
+        product_id smallint not null,
+        rating smallint not null,
+        review_text varchar(256),
+        review_date date,
+        foreign key(order_id) references orders(order_id),
+        foreign key(product_id) references products(product_id));
+        
+        
+    
+    
+
+
+
+
+
+
+
+
